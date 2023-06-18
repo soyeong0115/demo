b72b9a19f932325012f60cc714f4ad9bc8e86004
soyeong <mb535622@sookmyung.ac.kr>
2023-06-18 17:58:13 +0900

    백엔드 개발 14. ResponseEntity 로 HTTP Status 조작하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList; // 만약 자동으로 import 작성 안되면 직접 작성한다.
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,6 @@ import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("test")
@@ -24,7 +24,7 @@ public class TestController {
 	@GetMapping("/testRequestBody")
 	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO
 testRequestBodyDTO){
-		return "Hello World! ID"+testRequestBodyDTO.getId()+" Message : "+
+		return "Hello World! ID"+testRequestBodyDTO.getId()+" Message : "+ 
 testRequestBodyDTO.getMessage();
 	}
 	@GetMapping("/testResponseBody")
@@ -32,9 +32,32 @@ testRequestBodyDTO.getMessage();
 		List<String> list =new ArrayList<String>();
 		list.add("Hello World! I'm ResponseDTO");
 		list.add("See you!"
-						);
+							);
 		ResponseDTO<String> response =
 ResponseDTO.<String>builder().data(list).build();
 		return response;
 	}
+	
+	
+	@GetMapping("/testResponseEntityOk")
+	public ResponseEntity<?>testControllerResponseEntityOk(){
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 200!");
+		list.add("See you!");
+		ResponseDTO<String> response =
+ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+	
+	
+	@GetMapping("/testResponseEntityBad")
+	public ResponseEntity<?>testControllerResponseEntityBad(){
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 400!");
+		list.add("See you!");
+		ResponseDTO<String> response =
+ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.badRequest().body(response);
+	}
+	
 }
