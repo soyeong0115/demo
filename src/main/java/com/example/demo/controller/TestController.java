6aec404a0283c9c7ca9f7d9c17e3d81e27780059
soyeong <mb535622@sookmyung.ac.kr>
2023-06-18 18:11:43 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -20,13 +20,13 @@ public class TestController {
 	private TodoService service;
 	
 	@GetMapping("/test")
-	public ResponseEntity<?>testTodo(){
+	public ResponseEntity<?>testTodo(){	
 		String str = service.testService();
 		List<String> list =new ArrayList<>();
 		list.add(str);
 		ResponseDTO<String> response =
 ResponseDTO.<String>builder().data(list).build();
+
 		return ResponseEntity.ok().body(response);
 	}
-	
 }
